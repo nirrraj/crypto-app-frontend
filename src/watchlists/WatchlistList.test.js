5d40ba1b34030cefb2c580875e07383e777c8d1c
Niraj Singh <119704924+nirrraj@users.noreply.github.com>
2023-06-08 00:42:55 -0400

    Delete src directory

--- src/watchlists/WatchlistList.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import WatchlistList from "./WatchlistList";
-
-it("matches snapshot", function () {
-  const { asFragment } = render(<WatchlistList />);
-  expect(asFragment()).toMatchSnapshot();
-});
